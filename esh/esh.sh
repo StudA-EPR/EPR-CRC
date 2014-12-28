abd2bd22e1227f0e02378b8faba6f21409cd4491
Nils Sommer <mail@nilssommer.de>
2014-12-28 18:23:42 +0100

    Added support for multi-line-tags. There is a lot of duplication in the interpreter script. I will clean the code soon. And remember, error handling is still not implemented yet.

--- esh/esh.sh
+++ esh/esh.sh
@@ -23,26 +23,60 @@ then
 	exit 1
 fi
 
-# Iterate over the lines.
+multiline=false
+buffer=()
+
 while read line
 do
-	# Extract the shell command from the esh tag.
-	command=$(echo "$line" | sed -n 's/\(^.*\)\(<%\)\(.*\)\(%>\)\(.*$\)/\3/p')
-	
-	# Execute the shell command, if there is one.
-	if [ -n "$command" ]
+	if $multiline
 	then
-		output=$(eval $command)
+		esh_end=$(echo "$line" | sed -n '/<% esh_end %>/p')
+		if [ -n "$esh_end" ]
+		then
+			# Ausführen des Shellcodes
+			commands=''
+			for command in "${buffer[@]}"
+			do
+				commands=$commands$command'\n'
+			done
+			
+			# Execute the shell command, if there is one.
+			if [ -n "$commands" ]
+			then
+				#output=$(eval $commands)
+				output=$(echo "$commands" | sh)
+			else
+				output=''
+			fi
+			
+			echo "$output"
+							
+			multiline=false
+		else
+			buffer+=("$line")
+		fi
 	else
-		output=''
+		# Extract the shell command from the esh tag.
+		command=$(echo "$line" | sed -n 's/\(^.*\)\(<%\)\(.*\)\(%>\)\(.*$\)/\3/p')
+		if [ "$command" == " esh_begin " ]
+		then
+			multiline=true
+		else
+			# Execute the shell command, if there is one.
+			if [ -n "$command" ]
+			then
+				output=$(eval $command)
+			else
+				output=''
+			fi
+			# Sometimes, slashes appear in a commands output text. Escape them.
+			output=$(echo "$output" | sed 's/\//\\&/')
+	
+			# Substitute the original esh tag with its output.
+			substitution=$(echo "$line" | sed 's/<%.*%>/'"$output"'/')
+	
+			# Print that shit.
+			echo "$substitution"
+		fi
 	fi
-	
-	# Sometimes, slashes appear in a commands output text. Escape them.
-	output=$(echo "$output" | sed 's/\//\\&/')
-	
-	# Substitute the original esh tag with its output.
-	substitution=$(echo "$line" | sed 's/<%.*%>/'"$output"'/')
-	
-	# Print that shit.
-	echo "$substitution"
 done < "$1.html.esh"
