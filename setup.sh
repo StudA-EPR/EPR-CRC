15c378f1fd8c75e356d12da150306544622ddbee
Nils Sommer <mail@nilssommer.de>
2015-01-03 15:22:07 +0100

    Added setup.sh This script is used to set environment variables, set file permissions or copy files to other locations.

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#
+# Place all commands needed to set up the production environment here.
+#
+
+# Sets an environment variable with path to the location where camera
+# snapshots are saved to and read from.
+export SNAPSHOTS_DIR=/www/snapshots
