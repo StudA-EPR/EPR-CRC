0014dbf4674ee516ed839bd39b2b929a2ee4fc3f
Lukas Langenbacher <l.langenbacher@gmx.de>
2015-04-11 10:34:59 +0200

    implement restart and shutdown funtion

--- www/systemfunctions.php
+++ www/systemfunctions.php
@@ -6,28 +6,9 @@
 		switch($command)
 				{
 				case "restart": shell_exec('sudo /sbin/shutdown -r now'); break;
+				case "shutdown": shell_exec('sudo /sbin/shutdown -f now'); break;
 				
-				default: include("basis.php");
 			   }
 			   
 	}
 ?>
-
-
-<?php 
-			if(isset($_GET["page"]))
-{
-				switch($_GET["page"])
-				{
-				case "basis": include("basis.php"); break;
-				case "extended": include("extended.php"); break;
-				case "gallery": include("gallery.php"); break;
-				case "files": include("files.php"); break;
-				default: include("basis.php");
-			   }
-			 }
-			else
-				include("basis.php");
-
-
-			?>
