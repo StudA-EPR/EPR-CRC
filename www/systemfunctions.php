46161d8f7095a33febc3e0bb1f136d0f46fcc46f
Nils Sommer <mail@nilssommer.de>
2015-04-12 16:22:49 +0200

    Fix syntax error in systemfunctions.php

--- www/systemfunctions.php
+++ www/systemfunctions.php
@@ -3,13 +3,13 @@
 	if(isset($_GET["command"]))
 	{
 		$command = $_GET["command"];
-        
+
 		switch($command)
         {
             case "reboot":
                 shell_exec('reboot');
                 break;
-            case "shutdown"
+            case "shutdown":
                 shell_exec('halt');
                 break;
             default:
