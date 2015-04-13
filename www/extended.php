3cd4f90b215cb99e14d881d37ff092ee3973a794
Lukas Langenbacher <l.langenbacher@gmx.de>
2015-04-13 10:43:57 +0200

    add a pre-version of extended page

--- www/extended.php
+++ www/extended.php
@@ -23,9 +23,16 @@
 						<button type="button" class="btn btn-default"><i class="fa fa-caret-up"></i></button>
 						<button type="button"  class="btn btn-default"><i class="fa fa-caret-down"></i></button>
 					</div>  
-				</div>
+                   
+				</div>                
 				<p class="help-block">Anzahl der Serienbilder eingeben</p>
 			  </div>
+              <div class="col-md-4">
+                <label>
+                    <input type="checkbox" id="checkSerienbilder" value="">
+                    verwenden
+                 </label>
+              </div>
 			</div>
 			<!-- Intervall Input-->
 			<div class="form-group">
@@ -40,6 +47,60 @@
 				</div>
 				<p class="help-block">Zeit zwischen den Aufnahmen eingeben</p>
 			  </div>
+               <div class="col-md-4">
+                <label>
+                    <input type="checkbox" id="checkSerienbilder" value="">
+                    verwenden
+                 </label>
+              </div>
+			</div>
+            <!-- Verschlusszeit-->
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="appendedtext">Verschlusszeit</label>
+			  <div class="col-md-4">
+				<div class="input-group">
+                  <div class="input-group-btn">
+                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Action <span class="caret"></span></button>
+                    <ul class="dropdown-menu" role="menu" id="dropdownVerschlusszeit">
+                      <li><a href="#">mit</a></li>
+                      <li><a href="#">ausgelesenen</a></li>
+                      <li><a href="#">werten auffüllen</a></li>
+                    </ul>
+                  </div><!-- /btn-group -->
+                  <input type="text" class="form-control" aria-label="..." id="inputVerschlusszeit" >
+                </div><!-- /input-group -->
+				<p class="help-block">Verschlusszeit für die Aufnahme wählen</p>
+			  </div>
+               <div class="col-md-4">
+                <label>
+                    <input type="checkbox" id="checkSerienbilder" value="">
+                    verwenden
+                 </label>
+              </div>
+			</div>
+            <!-- Blende-->
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="appendedtext">Blende</label>
+			  <div class="col-md-4">
+				<div class="input-group">
+                  <div class="input-group-btn">
+                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Action <span class="caret"></span></button>
+                    <ul class="dropdown-menu" role="menu" id="dropdownBlende">
+                      <li><a href="#">mit</a></li>
+                      <li><a href="#">ausgelesenen</a></li>
+                      <li><a href="#">werten auffüllen</a></li>
+                    </ul>
+                  </div><!-- /btn-group -->
+                  <input type="text" class="form-control" aria-label="..." id="inputBlende" >
+                </div><!-- /input-group -->
+				<p class="help-block">Verschlusszeit für die Aufnahme wählen</p>
+			  </div>
+               <div class="col-md-4">
+                <label>
+                    <input type="checkbox" id="checkSerienbilder" value="">
+                    verwenden
+                 </label>
+              </div>
 			</div>
 			<!-- Button -->
 			<div class="form-group">
