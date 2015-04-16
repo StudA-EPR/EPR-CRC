7384f050649d055da19139dac317e6af4e12d69b
Lukas Langenbacher <l.langenbacher@gmx.de>
2015-04-16 16:13:53 +0200

    add function to load camera settings into form

--- www/extended.php
+++ www/extended.php
@@ -1,8 +1,10 @@
-		<form class="form-horizontal">  
+<div class="row "><table class="table table-bordered table-hover" id="cameraInfoTable"><thead><tr><th>Hersteller</th><th>Modell</th><th>Akkustand</th></tr></thead><tbody><tr></tr></tbody></table>
+</div>
+<hr>
+<form class="form-horizontal" action="extendedaction.php" method="post">
 			<fieldset>
 
-			<!-- Form Name -->
-			<legend>Erweiterte Einstellungen</legend>
+
 
 			<!-- Serienbilder Input-->
 			<div class="form-group">
@@ -40,62 +42,59 @@
 			  </div>
                <div class="col-md-4">
                 <label>
-                    <input type="checkbox" id="checkSerienbilder" value="">
+                    <input type="checkbox" id="checkIntervall" value="" name="">
                     verwenden
                  </label>
               </div>
 			</div>
+				<hr>
             <!-- Verschlusszeit-->
 			<div class="form-group">
 			  <label class="col-md-4 control-label" for="appendedtext">Verschlusszeit</label>
 			  <div class="col-md-4">			
-                  <select class="form-control">
-                      <option value="1">1</option>
-                      <option value="2">2</option>
-                      <option value="3">3</option>
-                      <option value="4">4</option>
-                      <option value="5">5</option>
+                  <select class="form-control" id="selectVerschlusszeit" name="verschlusszeit">
+
                    </select>               
 				<p class="help-block">Verschlusszeit für die Aufnahme wählen</p>
 			  </div>
-               <div class="col-md-4">
-                <label>
-                    <input type="checkbox" id="checkSerienbilder" value="">
-                    verwenden
-                 </label>
-              </div>
 			</div>
             
             <!-- Blende-->
             <div class="form-group">
 			  <label class="col-md-4 control-label" for="appendedtext">Blende</label>
 			  <div class="col-md-4">			
-                  <select class="form-control">
-                      <option value="1">1</option>
-                      <option value="2">2</option>
-                      <option value="3">3</option>
-                      <option value="4">4</option>
-                      <option value="5">5</option>
-                   </select>               
-				<p class="help-block">Verschlusszeit für die Aufnahme wählen</p>
+                  <select class="form-control" id="selectBlende" name="blende">
+
+                   </select>
+				<p class="help-block">Blendeneinstellung für die Aufnahme wählen</p>
 			  </div>
-               <div class="col-md-4">
-                <label>
-                    <input type="checkbox" id="checkSerienbilder" value="">
-                    verwenden
-                 </label>
-              </div>
 			</div>
+
+				<!-- ISO-->
+				<div class="form-group">
+					<label class="col-md-4 control-label" for="appendedtext">ISO</label>
+					<div class="col-md-4">
+						<select class="form-control" id="selectISO" name="iso">
+
+						</select>
+						<p class="help-block" id="isoinfo">ISO für die Aufnahme wählen</p>
+					</div>
+				</div>
+
 			<!-- Button -->
 			<div class="form-group">
-			  <label class="col-md-4 control-label" for="startCapture">Mit diesen Einstellungen aufnehmen</label>
+			  <label class="col-md-4 control-label" for="startCapture">Einstellungen in Kamera setzen</label>
 			  <div class="col-md-4">
-				<button id="startCapture" name="startCapture" class="btn btn-primary"><i class="fa fa-camera"></i> Auslösen</button>
+				<button id="setSettings" name="setSettings" class="btn btn-primary"><i class="fa fa-camera"></i> speichern</button>
 			  </div>
 			</div>
 
 			</fieldset>
 		</form>
+		<div id="gphoto-exception-wrapper-extended">
+		</div>
+
+
 
 
  
---
+++ www/js/extended.js
@@ -0,0 +1,255 @@
+$(document).ready(function() {
+    //start ajax request
+
+    //ladeKameraInfo();
+    //ladeVerschlusszeit();
+    //ladeBlende();
+    //ladeISO();
+
+   ladeKameraInfo();
+
+
+
+});
+
+var ladeKameraInfo = (function () {
+    var batterylevel;
+    var manufacturer;
+    var model;
+
+    $.ajax({
+        url: "/option-json.php",
+        data: {descriptor: "/main/status/manufacturer"},
+        method: "GET",
+        dataType: "json",
+    })
+        .done( function(data) {
+        manufacturer = data.current;
+            $('#cameraInfoTable > tbody > tr').empty();
+            $('#cameraInfoTable > tbody > tr').append('<td>'+ manufacturer + '</td>');
+
+            $.ajax({
+                url: "/option-json.php",
+                method: "GET",
+                data: {descriptor: "/main/status/cameramodel"},
+                dataType: "json",
+            })
+                .done( function(data) {
+                    model = data.current;
+                    $('#cameraInfoTable > tbody > tr').append('<td>'+ model + '</td>');
+                    $.ajax({
+                        url: "/option-json.php",
+                        data: {descriptor: "/main/status/batterylevel"},
+                        method: "GET",
+                        dataType: "json",
+                    })
+                        .done( function(data) {
+                            batterylevel = data.current;
+                            $('#cameraInfoTable > tbody > tr').append('<td>'+ batterylevel + '</td>');
+                            ladeVerschlusszeit();
+                        });
+
+                });
+
+
+    });
+    /*
+    $.ajax({
+        url: "/option-json.php?descriptor=%2Fmain%2Fstatus%2Fmanufacturer",
+        method: "GET",
+        dataType: "json",
+    })
+        .done( function(data) {
+            manufacturer = data.current;
+            console.log("hersteller: " + manufacturer);
+        });
+
+    $.ajax({
+        url: "/option-json.php?descriptor=%2Fmain%2Fstatus%2Fcameramodel",
+        method: "GET",
+        dataType: "json",
+    })
+        .done( function(data) {
+            model = data[0].current;
+        });
+     $('#cameraInfoTable > tbody').html('<tr><td>Kamera Hersteller: </td><td>'+ manufacturer + '</td></tr>'+
+                                '<tr><td>Kamera Modell: </td><td>'+ model + '</td></tr>'+
+                                '<tr><td>Akkustand: </td><td>'+ batterylevel + '</td></tr>');
+                                */
+});
+
+// function for shutterspeed settings
+var ladeVerschlusszeit = (function () {
+    $.ajax({
+        url: "/option-json.php",
+        data: {descriptor: "/main/capturesettings/shutterspeed2"},
+        method: "GET",
+        dataType: "json",
+        beforeSend: function () {
+            $selectboxVerschluss = $('#selectVerschlusszeit');
+            $selectboxVerschluss.prop('disabled', "disabled");
+            $selectboxVerschluss.html('<option>lade Einstellungen</option>');
+            // $('.snapshot-link img').css('opacity', '0.25');
+            //$('.snapshot-link').append('<i class="fa fa-spinner fa-spin snapshot-spinner"></i>');
+        }
+    })
+        .done(function (data) {
+            if (data.error === false) {
+                //data downloaded
+                //now data variable contains data in json format
+                var choices = data.choices;
+                var current = data.current;
+                $selectboxVerschluss.removeAttr('disabled');
+                $selectboxVerschluss.empty();
+
+                $.each(choices, function (key, value) {
+                    if (value == current) {
+                        $('#selectVerschlusszeit')
+                            .append($("<option></option>")
+                                .attr("value", key)
+                                .prop("selected", "selected")
+                                .text(value + " s"));
+                    }
+                    else {
+                        $('#selectVerschlusszeit')
+                            .append($("<option></option>")
+                                .attr("value", key)
+                                .text(value + " s"));
+                    }
+                });
+            } else {
+                // Error handling (show the exception):
+                $('#gphoto-exception-wrapper-extended').html(" \
+					<div class=\"alert alert-warning alert-dismissible\" role=\"alert\"> \
+            <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button> \
+            <strong>GPhotoException</strong> " + data.message + " \
+            <br /> \
+            <strong>Exit code: </strong> " + data.exitCode + " \
+            <br /> \
+            <strong>Stderr: </strong><br />" + data.output + " \
+        </div> \
+				");
+            }
+
+
+            ladeBlende();
+        });
+});
+
+// Function for apperture settings
+var ladeBlende = (function () {
+    $.ajax({
+        url: "/option-json.php",
+        data: {descriptor: "/main/capturesettings/f-number"},
+        method: "GET",
+        dataType: "json",
+        beforeSend: function () {
+            $selectboxBlende = $('#selectBlende');
+            $selectboxBlende.prop('disabled', "disabled");
+            $selectboxBlende.html('<option>lade Einstellungen </option>');
+        }
+    })
+        .done(function (blendedata) {
+
+            if (blendedata.error === false) {
+                //data downloaded
+                //now data variable contains data in json format
+                var choices = blendedata.choices;
+                var current = blendedata.current;
+                $selectboxBlende.removeAttr('disabled');
+                $selectboxBlende.empty();
+                $.each(choices, function (key, value) {
+                    if (value == current) {
+                        $('#selectBlende')
+                            .append($("<option></option>")
+                                .attr("value", key)
+                                .prop("selected", "selected")
+                                .text(value));
+                    }
+                    else {
+                        $('#selectBlende')
+                            .append($("<option></option>")
+                                .attr("value", key)
+                                .text(value));
+                    }
+                });
+            } else {
+                // Error handling (show the exception):
+                $('#gphoto-exception-wrapper-extended').html(" \
+					<div class=\"alert alert-warning alert-dismissible\" role=\"alert\"> \
+            <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button> \
+            <strong>GPhotoException</strong> " + blendedata.message + " \
+            <br /> \
+            <strong>Exit code: </strong> " + blendedata.exitCode + " \
+            <br /> \
+            <strong>Stderr: </strong><br />" + blendedata.output + " \
+        </div> \
+				");
+            }
+
+
+            ladeISO();
+        });
+});
+
+
+// Fucntion for ISO Values
+//FOR ISO, we first need to check whether autoiso is activated or not
+var ladeISO = (function () {
+    $.ajax({
+        url: "/option-json.php",
+        data: {descriptor: "/main/imgsettings/iso"},
+        method: "GET",
+        dataType: "json",
+        beforeSend: function () {
+            $selectboxISO = $('#selectISO');
+            $selectboxISO.prop('disabled', "disabled");
+            $selectboxISO.html('<option>lade Einstellungen </option>');
+        }
+    })
+        .done(function (data) {
+
+            if (data.error === false) {
+                //data downloaded
+                //now data variable contains data in json format
+                var choices = data.choices;
+                var current = data.current;
+                var isoauto = false; //TODO: check /main/imgsettings/isoauto whether current ="off"
+                if (isoauto == true) //check whether iso automatic is activated or not
+                {
+                    $('#isoinfo').text("Auswahl nicht mglich, da ISO-Automatik aktiviert");
+                }
+                else {
+                    $selectboxISO.removeAttr('disabled');
+                }
+                $selectboxISO.empty();
+                $.each(choices, function (key, value) {
+                    if (value == current) {
+                        $('#selectISO')
+                            .append($("<option></option>")
+                                .attr("value", key)
+                                .prop("selected", "selected")
+                                .text(value));
+                    }
+                    else {
+                        $('#selectISO')
+                            .append($("<option></option>")
+                                .attr("value", key)
+                                .text(value));
+                    }
+                });
+            } else {
+                // Error handling (show the exception):
+                $('#gphoto-exception-wrapper-extended').html(" \
+					<div class=\"alert alert-warning alert-dismissible\" role=\"alert\"> \
+            <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button> \
+            <strong>GPhotoException</strong> " + data.message + " \
+            <br /> \
+            <strong>Exit code: </strong> " + data.exitCode + " \
+            <br /> \
+            <strong>Stderr: </strong><br />" + data.output + " \
+        </div> \
+				");
+            }
+        });
+});
